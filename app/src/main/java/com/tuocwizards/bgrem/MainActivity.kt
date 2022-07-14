457344b0d806384dc5ce00affb4030988dc39b7e
RIoWaW <danilaf4101957@gmail.com>
2022-07-14 23:49:45 +0300

    Create bottom dialog window
    need to correct shapes

--- app/src/main/java/com/tuocwizards/bgrem/MainActivity.kt
+++ app/src/main/java/com/tuocwizards/bgrem/MainActivity.kt
@@ -2,10 +2,31 @@ package com.tuocwizards.bgrem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.info_page)
+        setContentView(R.layout.main_page)
+
+        val button : ImageButton = findViewById(R.id.photo_button)
+        button.setOnClickListener {
+            val bottomSheetDialog: BottomSheetDialog = BottomSheetDialog(this)
+            val bottomSheetView: View = layoutInflater
+                .inflate(R.layout.bottom_dialog_window, findViewById(R.id.bottom_dialog_container))
+
+            bottomSheetView.findViewById<Button>(R.id.cancel_button).setOnClickListener{
+                Toast.makeText(
+                    this, "tttt", Toast.LENGTH_SHORT
+                ).show()
+            }
+            bottomSheetDialog.setContentView(bottomSheetView)
+            bottomSheetDialog.show()
+        }
+
     }
 }
